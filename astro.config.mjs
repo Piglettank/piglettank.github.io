b4214cc1fc42066165d5b4c797bddac351d428f1
Piglettank <johanfallberg@hotmail.com>
2025-11-24 09:25:15 +0100

    React experiments, youtube, background

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,5 @@ export default defineConfig({
     build: {
         assets: 'astro',
     },
+    integrations: [react()]
 });
---
+++ src/components/ReactExperience.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Experience({addedString}: {addedString: string}) {
+    return <p>a paragraph {addedString}</p>
+}
